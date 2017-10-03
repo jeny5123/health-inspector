98bfa3f83cd95659eb8fe290ee42815223d0f699
Jenny Chau <jeny5123@gmail.com>
2017-10-02 20:58:51 -0700

    -_-

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+    return (
+        <div>
+            <h1>Hello Grace chohehehehehe!</h1>
+            <p> grace sucks :P </p>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker();
